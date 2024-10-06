0f37bae523133ea057f341fcfc7290811d096d73
SafyaAbouelfadel <safia.abf@gmail.com>
2024-10-06 15:35:24 -0700

    rsa challenge

--- factors.py
+++ factors.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-from sys import argv
 import math
-"""Factorize as many numbers as possible into a product of 2 smaller nums"""
 
+def print_factor(num):
+    a = 2
+    if (num % a == 0):
+        print("{:d}={:d}*{:d}".format(num, num // a, a))
+    a = 3
+    while a < num // 2:
+        if num % a == 0:
+            print("{}={}*{}".format(num, num // a, a))
+            break
+        a = a + 2
+        if a == (num // 2) + 1:
+            print("{}={}*{}".format(num, num, 1))
 
-def factory(num):
-    """get the factors of a number"""
-    if num % 2 == 0:
-        i = 2
-        print("{}={}*{}".format(num, int(num//i), i))
+def main():
+    from sys import argv, exit, stderr
+
+    if len(argv) != 2:
+        stderr.write("Usage: ./factors <file>\n")
+        exit()
+
+    try:
+        f = open(argv[1], "r")
+    except FileNotFoundError:
+        stderr.write("Could not find file {}, not exist\n".format(argv[1]))
     else:
-        sq = int(math.sqrt(num)) + 1
-        for i in range(3, sq, +2):
-            if num % i == 0:
-                print("{}={}*{}".format(num, int(num//i), i))
-                return
-            if num % (sq + i) == 0:
-                print("{}={}*{}".format(num, sq + i, int(num//(sq + i))))
-                return
-            if num % (sq - i) == 0:
-                print("{}={}*{}".format(num, sq - 1, int(num//(sq - i))))
-                return
+        while (True):
+            ln = f.readline()
+            if (not ln):
+                break
+            ln = int(ln)
+            print_factor(ln)
+
+    f.close()
 
 
-def factors(filename):
-    """read_file and print them out"""
-
-    with open(filename, encoding="utf-8") as my_file:
-        for i in my_file.readlines():
-            n = int(i)
-            result = factory(n)
-
-
-if __name__ == "__main__":
-    factors(argv[1])
+main()
