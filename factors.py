94aa5d5b1696d4253eefdc240ea618e2149b5501
SafyaAbouelfadel <safia.abf@gmail.com>
2024-10-06 10:54:21 -0700

    factor challenge

--- factors.py
+++ factors.py
@@ -1,18 +1,41 @@
 #!/usr/bin/python3
-from sys import argv
+import math
 
-with open(argv[1]) as f:
-    for ln in f:
-        n = int(ln)
-        print("{:d}=".format(n), end="")
-        if n % 2 == 0:
-            print("{}*2".format(n//2))
-            continue
-        for a in range(3, n, 2):
-            if n % a == 0:
-                factor = n//a
-                for k in range(3, factor, 2):
-                    if factor % k == 0 or a % k == 0:
-                        break
-                print("{}*{}".format(factor, a))
-                break
+def print_factor(num):
+    a = 2
+    if num % a == 0:
+        print("{:d}={:d}*{:d}".format(num, num // a, a))
+        return
+    a = 3
+    limit = math.isqrt(num)  # Efficient square root for large numbers
+    while a <= limit:
+        if num % a == 0:
+            print("{:d}={:d}*{:d}".format(num, num // a, a))
+            return
+        a += 2
+    # No factor found, so print the identity factorization
+    print("{:d}={:d}*1".format(num, num))
+
+def main():
+    from sys import argv, exit, stderr
+
+    if len(argv) != 2:
+        stderr.write("Usage: ./factors <file>\n")
+        exit(1)
+
+    try:
+        with open(argv[1], "r") as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    ln = int(line)  # Ensure it's a valid integer
+                    print_factor(ln)
+    except FileNotFoundError:
+        stderr.write("Could not find file {}, not exist\n".format(argv[1]))
+        exit(1)
+    except ValueError:
+        stderr.write("Invalid number format in file\n")
+        exit(1)
+
+if __name__ == "__main__":
+    main()
