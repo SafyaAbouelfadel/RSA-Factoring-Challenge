8c09a1a2b471a0b19e659fd94cb58cb86df337c4
SafyaAbouelfadel <safia.abf@gmail.com>
2024-10-05 18:25:49 -0700

    factor challenge

--- factors.py
+++ factors.py
@@ -4,9 +4,9 @@ import math
 def print_factor(num):
     i = 1
     while i * i <= num:
-        if (num % i == 0):
+        if num % i == 0:
             factor = num // i
-            print("{:d}={:d}*{:d}".format(num, factor, i))
+            print("{:d} = {:d} * {:d}".format(num, factor, i))
             break
         i += 1
 
@@ -16,21 +16,21 @@ def main():
 
     if len(argv) != 2:
         stderr.write("Usage: ./factors <file>\n")
-        exit()
+        exit(1)
 
     try:
-        fl = open(argv[1], "r")
+        with open(argv[1], "r") as fl:
+            for line in fl:
+                try:
+                    ln = int(line.strip())
+                    print_factor(ln)
+                except ValueError:
+                    stderr.write("Invalid integer in file: {}\n".format(line.strip()))
     except FileNotFoundError:
         stderr.write("Could not find file {}, not exist\n".format(argv[1]))
-    else:
-        while (True):
-            ln = fl.readline()
-            if (not ln):
-                break
-            ln = int(ln)
-            print_factor(ln)
-
-    fl.close()
+        exit(1)
 
 
-main()
+if __name__ == "__main__":
+    main()
+
