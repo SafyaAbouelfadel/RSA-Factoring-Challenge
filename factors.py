3119409fed76c5f7ae2c87a7661c5c3a719eeaa5
SafyaAbouelfadel <safia.abf@gmail.com>
2024-10-06 17:18:40 -0700

    rsa challenge ex

--- factors.py
+++ factors.py
@@ -1,27 +1,37 @@
-import sys
+#!/usr/bin/python3
 
-def factorize(n):
-    """Returns the first factor pair (p, q) such that n = p * q."""
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return i, n // i
-    return n, 1  # In case n is prime (though not necessary for this task)
+def print_factor(num):
+    if num < 2:
+        return  # No factors for numbers less than 2
 
-def main(file_path):
-    """Reads numbers from the file and factorizes each."""
+    for a in range(2, int(num**0.5) + 1):  # Start from 2 and check up to sqrt(num)
+        if num % a == 0:
+            factor = num // a
+            print("{:d} = {:d} * {:d}".format(num, factor, a))
+            return
+    print("{:d} = {:d} * 1".format(num, num))  # If no factors found, it must be prime
+
+
+def main():
+    from sys import argv, exit, stderr
+
+    if len(argv) != 2:
+        stderr.write("Usage: ./factors <file>\n")
+        exit(1)
+
     try:
-        with open(file_path, 'r') as file:
-            for line in file:
-                n = int(line.strip())
-                p, q = factorize(n)
-                print(f"{n}={q}*{p}")
+        with open(argv[1], "r") as f:
+            for ln in f:
+                ln = ln.strip()
+                try:
+                    num = int(ln)
+                    print_factor(num)
+                except ValueError:
+                    stderr.write("Invalid number: {}\n".format(ln))
     except FileNotFoundError:
-        print(f"Error: The file {file_path} was not found.")
-    except ValueError:
-        print("Error: The file contains non-integer values.")
+        stderr.write("Could not find file: {}\n".format(argv[1]))
+        exit(1)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: factors <file>")
-    else:
-        main(sys.argv[1])
+    main()
